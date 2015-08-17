6de8706a1c65ea4c23d69e7ba74089a377d700f4
Robert Murdoch <camera@robertmurdoch.com>
2015-08-17 17:15:49 +0000

    bug fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
+        end
+      end
+    end
+  end
+  
   private
 
   def sign_up_params
