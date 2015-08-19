b90763fa2c0fd02156fe0ba75d75c5af28235ead
Robert Murdoch <camera@robertmurdoch.com>
2015-08-19 18:40:37 +0000

    removed lines from app.rb

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module SciStartSaas
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
-    config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
